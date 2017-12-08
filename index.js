c34fc580b73af556080b6a30a850f81e889eef6e
John Kelvie <john.kelvie@gmail.com>
2017-12-08 15:50:58 -0500

    Initial commit of package

---
+++ index.js
@@ -0,0 +1,47 @@
+let MockDynamo = {
+    enable: function() {
+        const helper = require("alexa-sdk/lib/DynamoAttributesHelper");
+        helper.get = this.get;
+        helper.get.bind(this);
+
+        helper.set = this.set;
+        helper.set.bind(this);
+        this.reset();
+    },
+    get: function(table, userId, callback) {
+        console.log("UserID: " + userId);
+        let data = MockDynamo.userIdMap[userId];
+        if (data === undefined || data === null || isEmptyObject(data)) {
+            callback(null, {});
+        } else {
+            console.log(data.Item["mapAttr"].STATE);
+            callback(null, data.Item["mapAttr"]);
+        }
+    },
+    reset: function () {
+        MockDynamo.userIdMap = {};
+    },
+    set: function(table, userId, data, callback) {
+        const parameters = {
+            Item: {
+                mapAttr: data,
+                userId: userId,
+            },
+            TableName: table
+        };
+
+        console.log("Set: " + data.STATE);
+
+        MockDynamo.userIdMap[userId] = parameters;
+        callback(null, data);
+
+    },
+
+    userIdMap: {},
+};
+
+function isEmptyObject(obj) {
+    return !Object.keys(obj).length;
+}
+
+module.exports = MockDynamo;
